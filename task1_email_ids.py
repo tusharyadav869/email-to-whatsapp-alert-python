a6d083b71271b8b83126d9d5b562e4551c6cd241
Tushar Yadav <tusharyada8690@gmail.com>
2026-03-25 00:14:00 +0530

    extracting email ids and subject

--- task1_email_ids.py
+++ task1_email_ids.py
@@ -10,8 +10,6 @@ mail.select("inbox")
 status, messages = mail.search(None, "UNSEEN")
 email_ids = messages[0].split()
 
-print(email_ids)
-
 for e_id in email_ids:
     status, msg_data = mail.fetch(e_id, "(RFC822)")
 
@@ -19,4 +17,11 @@ for e_id in email_ids:
 
     subject, encoding = decode_header(msg["Subject"])[0]
 
+    if isinstance(subject, bytes):
+        subject = subject.decode(encoding if encoding else "utf-8")
     
+    sender = msg.get("From")
+
+    print("From: ", sender)
+    print("Subject: ", subject)
+    print("-"*40)
